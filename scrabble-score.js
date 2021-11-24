b436e1e83ec197c34838f2a9da4b4ae95203df05
jsarnecki <joshsarnecki@gmail.com>
2021-11-24 11:55:09 -0800

    exercism scrabble score first commit

---
+++ scrabble-score.js
@@ -0,0 +1,13 @@
+
+//export 
+const score = () => {
+  //Create object?  Including all letters along with their points
+  //Scrabble object - keys = the score , value = array of letters?  
+  //OR variables as the keys that are arrays with the letters, values with the score
+
+  //Break string into array
+  //Convert the object keys into their arrays...?  Create function / loop thru the arrKeys along the scrabbleArr
+
+  //Push matching values/key scores into array to add up later
+  //OR use reduce to add all the values that match to return
+};
