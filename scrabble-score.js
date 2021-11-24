fe64132dc9ef624243adf95652c1fa1c5dbab25a
jsarnecki <joshsarnecki@gmail.com>
2021-11-24 12:54:40 -0800

    Created array vairables containing letters sorted to their specific letter scores

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,9 +1,33 @@
 
+/*
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+*/
+
+
+let one = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
+let two = ["D", "G"];
+let three = ["B", "C", "M", "P"];
+let four = ["F", "H", "V", "W", "Y"];
+let five = ["K"];
+let eight = ["J", "X"];
+let ten = ["Q", "Z"];
+
+
+
 //export 
 const score = () => {
   //Create object?  Including all letters along with their points
   //Scrabble object - keys = the score , value = array of letters?  
   //OR variables as the keys that are arrays with the letters, values with the score
+
+  //Capital/Lowercase letter conditional required?
 
   //Break string into array
   //Convert the object keys into their arrays...?  Create function / loop thru the arrKeys along the scrabbleArr
